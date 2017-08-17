42348c8d87092842e4f5ebd0202b096e6c24708a
Matt Diephouse <matt@diephouse.com>
2017-08-17 18:03:04 -0400

    Add a GraphQL.Value type

---
+++ Source/GraphQL/GraphQL.Value.swift
@@ -0,0 +1,101 @@
+import Foundation
+
+extension GraphQL {
+    /// A value in a GraphQL document.
+    enum Value {
+        case variable(Variable)
+        case int(Int)
+        case float(Float)
+        case string(String)
+        case boolean(Bool)
+        case null
+        case `enum`(Name)
+        case list([Value])
+        case object([Name: Value])
+    }
+}
+
+extension GraphQL.Value: Hashable {
+    var hashValue: Int {
+        switch self {
+        case let .variable(variable):
+            return variable.hashValue
+        case let .int(int):
+            return int.hashValue
+        case let .float(float):
+            return float.hashValue
+        case let .string(string):
+            return string.hashValue
+        case let .boolean(boolean):
+            return boolean.hashValue
+        case .null:
+            return 0
+        case let .enum(`enum`):
+            return `enum`.hashValue
+        case let .list(list):
+            return list.map { $0.hashValue }.reduce(0, ^)
+        case let .object(object):
+            return object.map { $0.key.hashValue ^ $0.value.hashValue }.reduce(0, ^)
+        }
+    }
+    
+    static func ==(lhs: GraphQL.Value, rhs: GraphQL.Value) -> Bool {
+        switch (lhs, rhs) {
+        case let (.variable(lhs), .variable(rhs)):
+            return lhs == rhs
+        case let (.int(lhs), .int(rhs)):
+            return lhs == rhs
+        case let (.float(lhs), .float(rhs)):
+            return lhs == rhs
+        case let (.string(lhs), .string(rhs)):
+            return lhs == rhs
+        case let (.boolean(lhs), .boolean(rhs)):
+            return lhs == rhs
+        case (.null, .null):
+            return true
+        case let (.enum(lhs), .enum(rhs)):
+            return lhs == rhs
+        case let (.list(lhs), .list(rhs)):
+            return lhs == rhs
+        case let (.object(lhs), .object(rhs)):
+            return lhs == rhs
+        default:
+            return false
+        }
+    }
+}
+
+extension GraphQL.Value: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case let .variable(variable):
+            return variable.description
+        case let .int(int):
+            return int.description
+        case let .float(float):
+            return float.description
+        case let .string(string):
+            let substituted = string
+                .replacingOccurrences(of: "\u{005c}", with: "\\\\")
+                .replacingOccurrences(of: "\u{0022}", with: "\\\"")
+                .replacingOccurrences(of: "\u{002f}", with: "\\/")
+                .replacingOccurrences(of: "\u{0008}", with: "\\b")
+                .replacingOccurrences(of: "\u{000c}", with: "\\f")
+                .replacingOccurrences(of: "\u{000a}", with: "\\n")
+                .replacingOccurrences(of: "\u{000d}", with: "\\r")
+                .replacingOccurrences(of: "\u{0009}", with: "\\t")
+            return "\"\(substituted)\""
+        case let .boolean(boolean):
+            return boolean ? "true" : "false"
+        case .null:
+            return "null"
+        case let .enum(`enum`):
+            return `enum`.description
+        case let .list(list):
+            return "[" + list.map { $0.description }.joined(separator: " ") + "]"
+        case let .object(object):
+            return "{ " + object.map { "\($0.key): \($0.value)" }.joined(separator: ", ") + " }"
+        }
+    }
+}
+
---
+++ Tests/GraphQL/GraphQL.ValueTests.swift
@@ -0,0 +1,61 @@
+@testable import PersistQL
+import XCTest
+
+class GraphQLValueTests: XCTestCase {
+    func testVariableDescription() {
+        let variable = GraphQL.Variable(GraphQL.Name("mdiep"))
+        let value = GraphQL.Value.variable(variable)
+        XCTAssertEqual(value.description, variable.description)
+    }
+    
+    func testIntDescription() {
+        let value = GraphQL.Value.int(5)
+        XCTAssertEqual(value.description, "5")
+    }
+    
+    func testFloatDescription() {
+        let value = GraphQL.Value.float(3.14)
+        XCTAssertEqual(value.description, "3.14")
+    }
+    
+    func testStringDescription() {
+        let value = GraphQL.Value.string("a \u{005c} \u{0022} \u{002f} \u{0008} \u{000c} \u{000a} \u{000d} \u{0009} ✊️")
+        XCTAssertEqual(value.description, "\"a \\\\ \\\" \\/ \\b \\f \\n \\r \\t ✊️\"")
+    }
+    
+    func testBooleanDescription() {
+        XCTAssertEqual(GraphQL.Value.boolean(true).description, "true")
+        XCTAssertEqual(GraphQL.Value.boolean(false).description, "false")
+    }
+    
+    func testNullDescription() {
+        XCTAssertEqual(GraphQL.Value.null.description, "null")
+    }
+    
+    func testEnumDescription() {
+        XCTAssertEqual(GraphQL.Value.enum(GraphQL.Name("TEST")).description, "TEST")
+    }
+    
+    func testListDescription() {
+        let value = GraphQL.Value.list([
+            .variable(GraphQL.Variable(GraphQL.Name("mdiep"))),
+            .int(5),
+            .float(3.14),
+        ])
+        XCTAssertEqual(value.description, "[$mdiep 5 3.14]")
+    }
+    
+    func testObjectDescription() {
+        let value = GraphQL.Value.object([
+            GraphQL.Name("foo"): .int(5),
+            GraphQL.Name("bar"): .float(3.14),
+        ])
+        let permutations = [
+            "{ foo: 5, bar: 3.14 }",
+            "{ bar: 3.14, foo: 5 }",
+        ]
+        let description = value.description
+        XCTAssert(permutations.contains(description), "\(description)")
+    }
+}
+
