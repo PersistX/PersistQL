5790fc76064fb8459fc88b8c435e1236778a4005
Matt Diephouse <matt@diephouse.com>
2017-08-26 15:56:50 -0400

    Implement GraphQL type

--- Source/GraphQL/GraphQL.swift
+++ Source/GraphQL/GraphQL.swift
@@ -1,6 +1,29 @@
 import Foundation
 
-internal enum GraphQL {
+internal struct GraphQL {
     typealias Arguments = [Name: Value]
     typealias SelectionSet = Set<Selection>
+    
+    let operations: [Operation]
+    let fragments: [Fragment]
+}
+
+extension GraphQL: Hashable {
+    var hashValue: Int {
+        return operations.map { $0.hashValue }.reduce(0, ^)
+            ^ fragments.map { $0.hashValue }.reduce(0, ^)
+    }
+    
+    static func ==(lhs: GraphQL, rhs: GraphQL) -> Bool {
+        return lhs.operations == rhs.operations
+            && lhs.fragments == rhs.fragments
+    }
+}
+
+extension GraphQL: CustomStringConvertible {
+    var description: String {
+        let operations = self.operations.map { $0.description }
+        let fragments = self.fragments.map { $0.description }
+        return (operations + fragments).joined(separator: " ")
+    }
 }
--- Tests/GraphQL/GraphQLTests.swift
+++ Tests/GraphQL/GraphQLTests.swift
@@ -1,5 +1,35 @@
 @testable import PersistQL
 import XCTest
 
-class GraphQLTests: XCTestCase {
+class GraphQLDescriptionTests: XCTestCase {
+    func test() {
+        let operations = [
+            GraphQL.Operation(
+                type: .query,
+                name: "foo",
+                selectionSet: [
+                    .field(GraphQL.Field(name: "name"))
+                ]
+            ),
+            GraphQL.Operation(
+                type: .query,
+                name: "bar",
+                selectionSet: [
+                    .fragment("baz")
+                ]
+            )
+        ]
+        let fragments = [
+            GraphQL.Fragment(
+                name: "baz",
+                type: "User",
+                selectionSet: [
+                    .field(GraphQL.Field(name: "id")),
+                    .field(GraphQL.Field(name: "name")),
+                ]
+            )
+        ]
+        let graphQL = GraphQL(operations: operations, fragments: fragments)
+        XCTAssertEqual(graphQL.description, "query foo { name } query bar { ...baz } fragment baz on User { id name }")
+    }
 }
