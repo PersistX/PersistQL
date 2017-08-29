4c71e4603ec766a3b109150209d363a4e92deca5
Matt Diephouse <matt@diephouse.com>
2017-08-29 09:05:53 -0400

    Add Projection.value(forJSON: Any)

--- Source/Projection.swift
+++ Source/Projection.swift
@@ -1,6 +1,11 @@
+import Result
 import Schemata
 
 extension Projection {
+    struct DecodeError: Error {
+        let keyPath: PartialKeyPath<Model>
+    }
+    
     var selectionSet: GraphQL.SelectionSet {
         let schema = Model.schema
         let selections = keyPaths
@@ -17,5 +22,59 @@ extension Projection {
             }
             .map(GraphQL.Selection.field)
         return GraphQL.SelectionSet(selections)
+    }
+    
+    func value(forJSON json: Any) -> Result<Value, DecodeError> {
+        var values: [PartialKeyPath<Model>: Any] = [:]
+        
+        let schema = Model.schema
+        for keyPath in keyPaths {
+            let error = DecodeError(keyPath: keyPath)
+            var value = json
+            for property in schema.properties(for: keyPath) {
+                guard let object = value as? [String: Any] else { return .failure(error) }
+                let newValue = object[property.path]
+                
+                switch property.type {
+                case .toMany:
+                    fatalError()
+                    
+                case .toOne:
+                    if let newValue = newValue {
+                        value = newValue
+                    } else {
+                        return .failure(error)
+                    }
+                    
+                case let .value(type, nullable: nullable):
+                    let primitive: Primitive
+                    if let newValue = newValue {
+                        if let newValue = newValue as? Double {
+                            primitive = .double(newValue)
+                        } else if let newValue = newValue as? Int {
+                            primitive = .int(newValue)
+                        } else if let newValue = newValue as? String {
+                            primitive = .string(newValue)
+                        } else {
+                            return .failure(error)
+                        }
+                    } else if nullable {
+                        primitive = .null
+                    } else {
+                        return .failure(error)
+                    }
+                    
+                    switch type.anyValue.decode(primitive) {
+                    case let .success(newValue):
+                        value = newValue
+                    case .failure:
+                        return .failure(error)
+                    }
+                }
+            }
+            values[keyPath] = value
+        }
+        
+        return .success(makeValue(values))
     }
 }
--- Tests/ProjectionTests.swift
+++ Tests/ProjectionTests.swift
@@ -1,8 +1,8 @@
 @testable import PersistQL
 import XCTest
 
-class ProjectionTests: XCTestCase {
-    func test_selectionSet() {
+class ProjectionSelectionSetTests: XCTestCase {
+    func test() {
         let expected: GraphQL.SelectionSet = [
             .field(GraphQL.Field(name: "title")),
             .field(GraphQL.Field(
@@ -14,5 +14,26 @@ class ProjectionTests: XCTestCase {
             )),
         ]
         XCTAssertEqual(BookViewModel.projection.selectionSet, expected)
+    }
+}
+
+class ProjectionValueForJSONTests: XCTestCase {
+    func testToOne() {
+        let expected = BookViewModel(
+            title: "The Martian Chronicles",
+            authorID: Author.ID("ray-bradbury"),
+            authorName: "Ray Bradbury"
+        )
+        
+        let json: Any = [
+            "title": expected.title,
+            "author": [
+                "id": expected.authorID.string,
+                "name": expected.authorName,
+            ]
+        ]
+        let actual = BookViewModel.projection.value(forJSON: json)
+        
+        XCTAssertEqual(actual.value, expected)
     }
 }
